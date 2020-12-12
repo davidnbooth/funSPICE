74c01ad5b80bb1af0dd95137165a9000cdeb6a8e
gitconfig--globaluser.name <user.email>
2020-12-12 13:15:42 -0500

    moving to github

--- circuitSimHelpers.py
+++ circuitSimHelpers.py
@@ -1,3 +1,39 @@
+import numpy as np
+#np.random.seed(111)
+### BFS for supernodes
+#  Each node knows about its edges and has a value
+#  Each edge has a directed value and two nodes which it knows about.
+#  A set of nodes which have been explored is maintained
+#
+def supernodeInternalGraph(nodes, edges):
+    explored = set()
+    queue = []
+
+    # pick a root and define its value as reference (zero)
+    root = np.random.choice(list(nodes.keys()))
+    queue.append(root)
+    nodes[root].V = 0
+    # pull a node
+    while len(queue) > 0:
+        nid = queue.pop()
+        explored.add(nid)
+        # for each edge, find the attached nodes
+        for eid in nodes[nid].elemSet:
+            attachedNode = edges[eid].nnode if eid > 0 else edges[abs(eid)].pnode  # get attached node from other side of element
+            val = nodes[nid].V - np.sign(eid) * edges[abs(eid)].value  # value attached node should get
+            if attachedNode in explored:  # attached node has been fully explored
+                if abs(val - nodes[attachedNode].V) > 0.001:
+                    print('Invalid Graph')
+                    print(attachedNode + ' has value ' + str(nodes[attachedNode].V) + ' and node ' + nid + ' is trying to assign it ' + str(val))
+            elif nodes[attachedNode].V is not None:  # attached node has not been visited but has been given a value by another attached node
+                if abs(val - nodes[attachedNode].V) > 0.001:
+                    print('Invalid Graph')
+                    print(attachedNode + ' has value ' + str(nodes[attachedNode].V) + ' and node ' + nid + ' is trying to assign it ' + str(val))
+            else:
+                queue.append(attachedNode)
+                nodes[attachedNode].V = val
+    return {nid: nodes[nid].V for nid in nodes.keys()}
+
 
 def clumpClumps(inputList):     # This code is contributed by Abhishek Valsan
     # Python program to print connected
@@ -78,3 +114,32 @@ def secantUpdate(f, initialGuess, maxiterations, tolerance):
 if __name__ == '__main__':
     inputList = ((0, 1), (2, 1), (2, 40), (5, 6), (6, 8), (3, 7))
     print(clumpClumps(inputList))
+
+    #
+    #     Example:
+    #
+    #        +e1-
+    #     A ----- B
+    #   + |
+    #   e2|
+    #   - |-e3+   -e4+
+    #     C----E----F
+    #  +  |   /  -
+    #   e6|  / e5
+    #  -  | /    +
+    #     D
+    #
+    #  Should be:  A
+    exampleNodes = {'A': {1, 2}, 'B': {-1,}, 'C': {-2, -3, 6}, 'D': {5, -6}, 'E': {3, -4, -5}, 'F': {4, }}
+    exampleEdges = {1: ('A', 'B', 4), 2: ('A', 'C', 3), 3: ('E', 'C', 2), 4: ('F', 'E', 2), 5: ('D', 'E', 2), 6: ('C', 'D', -4)}
+    exampleNodes = {'N002': {50001,}, 'N012': {-50001}}
+
+    from circuitSimParts import Node
+    from circuitSimParts import Elem
+    nodes = {k: Node(k) for k in exampleNodes.keys()}
+    for nid, node in nodes.items():
+        node.elemSet = exampleNodes[nid]
+    edges = {k: Elem(k, v[0], v[1], v[2]) for k, v in exampleEdges.items()}
+
+    edges = {50001: Elem('V01', 'N002', 'N012', 10.)}
+    print(supernodeInternalGraph(nodes, edges))
--- circuitSimParts.py
+++ circuitSimParts.py
@@ -1,29 +1,91 @@
 import numpy as np
+from circuitSimHelpers import supernodeInternalGraph
+
+
+class Elem:
+    def __init__(self, name, pnode, nnode, value):
+        eTypeDict = {'R': 10000, 'C': 20000, 'L': 30000, 'I': 40000, 'V': 50000}
+        try:
+            self.typ = name[0].upper()
+            self.id = eTypeDict[self.typ] + int(name[1:])
+            self.name = name
+        except Exception:
+            print('Element Identifier not understood: ' + self.name)
+        self.nnode = nnode
+        self.pnode = pnode
+        self.value = float(value)
+        self.current = None
+
+    def prnt(self):
+        print(str(self.name) + ' ' + str(self.pnode) + ' ' + str(self.nnode) + ' ' + str(self.value))
+
+
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.elemSet = set()
+        self.V = 0
+
+
+class Supernode:
+    def __init__(self, id, nodeCol, nDict, eDict):
+        self.id = id
+        self.nodeCol = nodeCol
+        self.elemSet = set()
+        self.BV = 0
+        for nid in self.nodeCol:
+                self.elemSet.update(nDict[nid].elemSet)
+        # take out any elements that appear twice (ones that are inside the supernode)
+        posSet = {item for item in self.elemSet if item > 0}
+        negSet = {abs(item) for item in self.elemSet if item < 0}
+        self.internalElemset = posSet.intersection(negSet)
+        elems = self.elemSet - self.internalElemset - {-item for item in self.internalElemset}
+        self.elemSet = dict()
+        for eid in elems:
+            if eid > 0:
+                self.elemSet[eid] = eDict[eid].pnode
+            else:
+                self.elemSet[eid] = eDict[-eid].nnode
+        #define internalNodes - a list of node objects without references to elems outside the supernode and also internalElems
+        self.internalNodes = dict()
+        for nid in self.nodeCol:
+            self.internalNodes[nid] = Node(nid)
+            self.internalNodes[nid].elemSet = nDict[nid].elemSet.intersection(self.internalElemset) | nDict[nid].elemSet.intersection({-item for item in self.internalElemset})
+            self.internalNodes[nid].V = None
+        self.internalElems = dict()
+        for eid in self.internalElemset:
+            elem = Elem(eDict[eid].name, eDict[eid].pnode, eDict[eid].nnode, 0)
+            self.internalElems[eid] = elem
+            if elem.typ == 'V':
+                self.internalElems[eid].value = eDict[eid].value
+        self.nUpdate = supernodeInternalGraph(self.internalNodes, self.internalElems)
+
+
+    def updateNodes(self, nDict):
+        vchange = 0
+        for node in self.nodeCol:
+            oldV = nDict[node].V
+            nDict[node].V = self.BV + self.nUpdate[node]
+            vchange = max(vchange, abs(oldV - nDict[node].V))
+        return vchange
+
+    def prnt(self):
+        print('Node ' + str(self.id) + ':')
+        print('    Nodecol: ' + ''.join([str(n)+', ' for n in self.nodeCol]))
+        print('    BV: ' + str(self.BV))
+        print('    Elemset: ')
+        print('            ' + ''.join([str(k) + ': ' + str(v) + ', ' for k, v in self.elemSet.items()]))
+        print('    nUpdate:')
+        print('            ' + ''.join([str(k) + ': ' + str(v) + ', ' for k, v in self.nUpdate.items()]))
+        print('    Internal Elements:')
+        for elem in self.internalElems.values():
+            print('        ' + str(elem.id) + ':  ' + str(elem.pnode) + ' ' + str(elem.nnode))
+        print('    Internal Nodes:')
+        for node in self.internalNodes.values():
+            print('        ' + str(node.id) + ':  ' + ''.join([str(eid)+', ' for eid in node.elemSet]))
+
 
 def circuitPreprocess(filepath):
-    class Elem:
-        def __init__(self, name, pnode, nnode, value):
-            try:
-                self.typ = name[0].upper()
-                self.id = {'R': 10000, 'C': 20000, 'L': 30000, 'I': 40000, 'V': 50000}[self.typ] + int(name[1:])
-            except Exception:
-                print('Element Identifier not understood: ' + self.name)
-            # strip leading Ns from node numbers
-            self.nnode = nnode
-            self.pnode = pnode
-            self.value = float(value)
-            self.name = name
-            self.current = None  # for postprocessing
-
-        def prnt(self):
-            print(str(self.name) + ' ' + str(self.pnode) + ' ' + str(self.nnode) + ' ' + str(self.value))
-
-    class Node:
-        def __init__(self, id):
-            self.id = id
-            self.elemSet = set()
-            self.V = 0
-
     # Reading in the file with comments
     with open(filepath, 'r') as f:
         inputCirc = []
